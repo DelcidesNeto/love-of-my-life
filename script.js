2bde8f361ba57f01185b9b178cac989e0959b9cf
DelcidesNeto <115945133+DelcidesNeto@users.noreply.github.com>
2025-03-18 23:59:08 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var TotalDeImagens = [4]
+var TotalDeImagens = [8]
 // Função para criar os corações flutuando
 // Função para criar os corações descendo
 // Função para criar os corações descendo do topo da página
